4ea9beb3894151dcb8f5ddf8320184927f56be52
Laura Cole <lauracole1900@comcast.net>
2021-07-01 19:10:31 -0600

    back-end files

---
+++ server/models/ToDo.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require('mongoose');
+
+const toDoSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  due: {
+    type: Date
+  },
+  created_At: {
+    type: Date,
+    default: Date.now,
+    required: true
+  }
+})
+
+const ToDo = model("ToDo", toDoSchema);
+
+module.exports = ToDo;
