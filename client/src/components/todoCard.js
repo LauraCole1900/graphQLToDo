3a00747b79c2c15b50e18687d00ac6b6de893f1e
Laura Cole <lauracole1900@comcast.net>
2021-07-07 20:34:46 -0600

    changed placement of todo card buttons

--- client/src/components/todoCard.js
+++ client/src/components/todoCard.js
@@ -15,31 +15,31 @@ const ToDoCard = (props) => {
 
   return (
     <>
-    {props.toDos?.map(todo => (
-      <Card className="todoCard" key={todo._id}>
-        <Card.Header className="cardHeader">
-          <Row>
-            <Col sm={7}>
-              <h2>{todo.name}</h2>
-            </Col>
-            <Col sm={2}>
-              <Button data-toggle="popover" title="Edit" className="button" onClick={handleEdit}>Edit</Button>
-            </Col>
-            <Col sm={2}>
-              <Button data-toggle="popover" title="Delete" className="button" onClick={handleDelete}>Delete</Button>
-            </Col>
-          </Row>
-        </Card.Header>
-        <Card.Body className="cardBody">
-          <Row>
-            <Col sm={12}>
-              <p>Due: {todo.due}</p>
-              <p>Description: {todo.description}</p>
-            </Col>
-          </Row>
-        </Card.Body>
-      </Card>
-    ))}
+      {props.toDos?.map(todo => (
+        <Card className="todoCard" key={todo._id}>
+          <Card.Header className="cardHeader">
+            <Row>
+              <Col sm={12}>
+                <h2 className="cardTitle">{todo.name}</h2>
+              </Col>
+            </Row>
+          </Card.Header>
+          <Card.Body className="cardBody">
+            <Row>
+              <Col sm={12}>
+                <p>Due: {todo.due}</p>
+                <p>Description: {todo.description}</p>
+              </Col>
+            </Row>
+            <Row>
+              <Col sm={12} className="cardBtns">
+                <Button data-toggle="popover" title="Edit" className="button" onClick={handleEdit}>Edit</Button>
+                <Button data-toggle="popover" title="Delete" className="button" onClick={handleDelete}>Delete</Button>
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      ))}
     </>
   )
 }
