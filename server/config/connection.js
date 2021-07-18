734fa20a11fb08c7a9fe9cac0e7f916c5d17f7fa
Laura Cole <lauracole1900@comcast.net>
2021-07-17 19:13:23 -0600

    working on back-end

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/gql-to-do', {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gql-to-do", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
