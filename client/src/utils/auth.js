defe7583cca1bbeddd42c9baf8cfc37ffcf8db83
Laura Cole <lauracole1900@comcast.net>
2022-03-02 22:07:10 -0700

    fixed login redirect

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -34,7 +34,7 @@ class AuthService {
   // POST user token to localStorage
   login(idToken) {
     localStorage.setItem("id_token", idToken);
-    window.location.assign("/members");
+    window.location.assign("/");
   };
 
   // DELETE user token and profile data from localStorage,
